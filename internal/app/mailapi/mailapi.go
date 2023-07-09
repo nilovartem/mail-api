a917ba1e1544ab5ef2d704db74817d89174b314a
Artem <nilovtema@yandex.ru>
2023-07-10 00:41:53 +0300

    add some logging

--- internal/app/mailapi/mailapi.go
+++ internal/app/mailapi/mailapi.go
@@ -55,10 +55,13 @@ func (s *Server) serveHTTP(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch {
 		case r.Method == http.MethodPost && postHandlerRe.MatchString(r.URL.Path):
+			s.logger.Infoln("[serveHTTP] got request to endpoint #1")
 			next.ServeHTTP(w, r)
 		case r.Method == http.MethodGet && getHandlerRe.MatchString(r.URL.Path):
+			s.logger.Infoln("[serveHTTP] got request to endpoint #2")
 			next.ServeHTTP(w, r)
 		default:
+			s.logger.Infoln("[serveHTTP] got invalid request")
 			http.NotFound(w, r)
 		}
 	})
@@ -69,6 +72,7 @@ func (s *Server) staticAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		usernameURL := strings.Trim(r.URL.Path, "/")
 		username, password, ok := r.BasicAuth()
+		s.logger.Infof("[staticAuth] username: %s, password : %s, username in URL %s\n", username, password, usernameURL)
 		if ok && username == usernameURL {
 			if passwordHash, found := s.config.Users[username]; found {
 				if passwordHash == sha256.Sum256([]byte(password)) {
@@ -85,8 +89,10 @@ func (s *Server) staticAuth(next http.Handler) http.Handler {
 // postHandler responds with link - old or new
 func (s *Server) postHandler(w http.ResponseWriter, r *http.Request) {
 	username := strings.Trim(r.URL.Path, "/")
+	s.logger.Infof("[postHandler] username: %s\n", username)
 	link, found := s.storage.GetLink(username)
 	if !found {
+		s.logger.Infoln("[postHandler] link not found, creating new link")
 		link = s.storage.Add(username)
 		go func() {
 			ticker := time.NewTicker(s.config.TTL)
@@ -96,13 +102,16 @@ func (s *Server) postHandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}()
 	}
+	s.logger.Infof("[postHandler] link: %s\n", link)
 	fmt.Fprint(w, "/get/"+link)
 }
 
 // getHandler responds to valid link with zip
 func (s *Server) getHandler(w http.ResponseWriter, r *http.Request) {
 	link := strings.Replace(r.URL.Path, "/get/", "", -1)
+	s.logger.Infof("[getHandler] link: %s\n", link)
 	if u, found := s.storage.GetUser(link); found {
+		s.logger.Infof("[getHandler] found owner's username: %s\n", u.Username)
 		zip, err := u.Zip(filepath.Join(s.config.Mailbox, u.Username), u.Username, s.config.PDF)
 		if err != nil {
 			http.Error(w, "zip error", http.StatusInternalServerError)
@@ -114,6 +123,7 @@ func (s *Server) getHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write(zip)
 		return
 	}
+	s.logger.Infof("[getHandler] link %s is expired\n", link)
 	http.Error(w, "link expired", http.StatusBadRequest)
 }
 
